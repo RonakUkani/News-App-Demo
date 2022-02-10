65e86ed83d61e96a468b733a5c6785e37aa8afdf
ronak <ukanironak@gmail.com>
2022-02-10 22:57:52 +0530

    add: added NewsList API and bind data to top and popular news adapter

--- app/src/main/java/com/newsappdemo/ui/activity/NewsListActivity.kt
+++ app/src/main/java/com/newsappdemo/ui/activity/NewsListActivity.kt
@@ -1,11 +1,17 @@
 package com.newsappdemo.ui.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.newsappdemo.R
+import com.newsappdemo.data.AppResponse
+import com.newsappdemo.ui.adapter.NewsListAdapter
 import com.newsappdemo.utils.viewModelProvider
 import com.newsappdemo.viewmodel.NewsListViewModel
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_news_list.*
 import javax.inject.Inject
 
 class NewsListActivity : DaggerAppCompatActivity() {
@@ -13,12 +19,44 @@ class NewsListActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private var pageNumber = 1
+    private var newsList: MutableList<AppResponse.Article> = mutableListOf()
+    private val adapter = NewsListAdapter(newsList) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_list)
         newsListViewModel = viewModelProvider(viewModelFactory)
-//        newsListViewModel.getNewsList(pageNumber.toString())
+        setAdapter()
+        newsListViewModel.getNewsList(pageNumber.toString())
+        observeData()
+    }
 
+    private fun setAdapter() {
+        recyclerViewNews.adapter = adapter
+        recyclerViewNews.layoutManager = LinearLayoutManager(this)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun observeData() {
+        newsListViewModel.newsListSuccess.observe(this, Observer {
+            if (it.status.equals("ok")) {
+                if (newsList.isEmpty()) {
+                    for (i in 0 until it.articles.size) {
+                        if (i == 0) {
+                            it.articles[0].isShowTitle = true
+                            it.articles[0].newsEnum = AppResponse.Article.NewsEnum.TOP_NEWS
+                        } else if (i == 1) {
+                            it.articles[1].isShowTitle = true
+                        }
+                    }
+                }
+                newsList.addAll(it?.articles!!)
+            }
+            adapter.notifyDataSetChanged()
+        })
+
+        newsListViewModel.newsListFailure.observe(this, Observer {
+
+        })
     }
 }
--- app/src/main/java/com/newsappdemo/utils/Constants.kt
+++ app/src/main/java/com/newsappdemo/utils/Constants.kt
@@ -4,4 +4,8 @@ object Constants {
     const val KEY_COUNTRY = "us"
     const val KEY_CATEGORY = "business"
     const val KEY_DEFAULT_PAGE_SIZE = "10"
+
+    const val TOP_NEWS = 1
+    const val POPULAR_NEWS = 2
+    const val NEWS_DETAIL = 3
 }
